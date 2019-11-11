885e91e2ebecf2a443d1cd545a900636ff12d6eb
Your Name <apoorva.thirupathi@research.iiit.ac.in>
2019-11-11 12:29:39 +0530

    added report functions

--- report.py
+++ report.py
@@ -1,149 +1,85 @@
 import subprocess as sp
 import pymysql
 import pymysql.cursors
-
+from datetime import date
 
 
 #Update and show fns
 
-def updateSalaryProfit():
+def updateManagerSalary():
     global cur
     row = {}
-    print("Enter new employee's details: ")
-    row["HRID"] = input("HR ID: ")
-    row["Name"] = input("Name: ")
-    row["Email"] = input("Email Address: ")
-    row["Phone"] = input("Phone Numbers: ").split(" ")
-    row["StoreID"] = input("Store ID: ")
-    row["Type"] = input("Type of Employee: ")
-    row["Salary"] = float(input("Salary: "))
-
-    for i in range(len(row["Phone"])):
-	    query1 = "INSERT INTO HR(HRID, Name, Email, PhoneNumber) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["Name"], row["Email"], row["Phone"][i])
-    	cur.execute(query1)
-    	con.commit()
-
-    query2 = "INSERT INTO EMPLOYEE(HRID, StoreID, TypeOfEmployee, SALARY) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["StoreID"], row["Type"], row["SALARY"])
-
-    cur.execute(query2)
-    con.commit()
+    query1 = "SELECT HRID FROM EMPLOYEE WHERE TypeOfEmployee='Manager'"
+    cur.execute(query1)
+    Managers=cur.fetchall()
+    for i in range(len(Managers)):
+        query="SELECT SUM(TotalProfit) FROM REGISTER WHERE ManagerHRID='%s'" %(Managers[i])
+        cur.execute()
+        managerSalary=cur.fetchall()
+        managerSalary=float(managerSalary[0])
+        query2="UPDATE EMPLOYEE SET Salary='%d' WHERE HRID='%s'" %(managerSalary*0.2, Managers[i])
+        cur.execute(query2)
+        con.commit()
     return
 
 def reportProfit():
     global cur
-    row = {}
-    print("Enter new employee's details: ")
-    row["HRID"] = input("HR ID: ")
-    row["Name"] = input("Name: ")
-    row["Email"] = input("Email Address: ")
-    row["Phone"] = input("Phone Numbers: ").split(" ")
-    row["StoreID"] = input("Store ID: ")
-    row["Type"] = input("Type of Employee: ")
-    row["Salary"] = float(input("Salary: "))
+    storeid=input("Enter Store ID")
+    print("The Store with ID ",storeid ,"has profits of", )
 
-    for i in range(len(row["Phone"])):
-	    query1 = "INSERT INTO HR(HRID, Name, Email, PhoneNumber) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["Name"], row["Email"], row["Phone"][i])
-    	cur.execute(query1)
-    	con.commit()
-
-    query2 = "INSERT INTO EMPLOYEE(HRID, StoreID, TypeOfEmployee, SALARY) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["StoreID"], row["Type"], row["SALARY"])
-
-    cur.execute(query2)
-    con.commit()
+    query = "SELECT SUM(TotalProfit) FROM REGISTER WHERE StoreID = '%s'"
+    cur = execute(query)
+    blah = cur.fetchall()
+    blah=int(blah[0])
+    print blah
     return
 
 def showStoreItems():
     global cur
-    row = {}
-    print("Enter new employee's details: ")
-    row["HRID"] = input("HR ID: ")
-    row["Name"] = input("Name: ")
-    row["Email"] = input("Email Address: ")
-    row["Phone"] = input("Phone Numbers: ").split(" ")
-    row["StoreID"] = input("Store ID: ")
-    row["Type"] = input("Type of Employee: ")
-    row["Salary"] = float(input("Salary: "))
+    storeID=input("Enter Store ID: ");
 
-    for i in range(len(row["Phone"])):
-	    query1 = "INSERT INTO HR(HRID, Name, Email, PhoneNumber) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["Name"], row["Email"], row["Phone"][i])
-    	cur.execute(query1)
-    	con.commit()
-
-    query2 = "INSERT INTO EMPLOYEE(HRID, StoreID, TypeOfEmployee, SALARY) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["StoreID"], row["Type"], row["SALARY"])
+    query2 = "SELECT * FROM HAS WHERE StoreID='%s'" %(storeID)
 
     cur.execute(query2)
-    con.commit()
+    blah = cur.fetchall()
+    for value in blah:
+        print value
+
     return
 
 def checkNowSales():
     global cur
-    row = {}
-    print("Enter new employee's details: ")
-    row["HRID"] = input("HR ID: ")
-    row["Name"] = input("Name: ")
-    row["Email"] = input("Email Address: ")
-    row["Phone"] = input("Phone Numbers: ").split(" ")
-    row["StoreID"] = input("Store ID: ")
-    row["Type"] = input("Type of Employee: ")
-    row["Salary"] = float(input("Salary: "))
-
-    for i in range(len(row["Phone"])):
-	    query1 = "INSERT INTO HR(HRID, Name, Email, PhoneNumber) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["Name"], row["Email"], row["Phone"][i])
-    	cur.execute(query1)
-    	con.commit()
-
-    query2 = "INSERT INTO EMPLOYEE(HRID, StoreID, TypeOfEmployee, SALARY) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["StoreID"], row["Type"], row["SALARY"])
-
-    cur.execute(query2)
-    con.commit()
+    today=date.today()
+    query="SELECT * FROM SALE WHERE '%s' BETWEEN StartDate AND EndDate"
+    cur.execute()
+    salesNow=cur.fetchall()
+    for i in range(len(salesNow)):
+        print(salesNow[i])
     return
 
 
 def checkSuperviseeSalaryProfit():
     global cur
-    row = {}
-    print("Enter new employee's details: ")
-    row["HRID"] = input("HR ID: ")
-    row["Name"] = input("Name: ")
-    row["Email"] = input("Email Address: ")
-    row["Phone"] = input("Phone Numbers: ").split(" ")
-    row["StoreID"] = input("Store ID: ")
-    row["Type"] = input("Type of Employee: ")
-    row["Salary"] = float(input("Salary: "))
+    id=input("Enter your HRID: ")
+    empid=input("Enter supervisee's HRID: ")
 
-    for i in range(len(row["Phone"])):
-	    query1 = "INSERT INTO HR(HRID, Name, Email, PhoneNumber) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["Name"], row["Email"], row["Phone"][i])
-    	cur.execute(query1)
-    	con.commit()
-
-    query2 = "INSERT INTO EMPLOYEE(HRID, StoreID, TypeOfEmployee, SALARY) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["StoreID"], row["Type"], row["SALARY"])
-
-    cur.execute(query2)
-    con.commit()
+    query = "SELECT A.Salary FROM EMPLOYEE A, SUPERVISES B WHERE B.CashierHRID='%s' AND B.SupervisorHRID='%s' AND B.CashierHRID=A.HRID" %(empid, id)
+    cur = execute(query)
+    superviseeSalary = cur.fetchall()
+    for i in range(len(superviseeSalary)):
+        print(superviseeSalary[i])
+    if len(superviseeSalary) == 0:
+        print("This employee is not your supervisee")
     return
-
+ 
 def checkMySalary():
     global cur
-    row = {}
-    print("Enter new employee's details: ")
-    row["HRID"] = input("HR ID: ")
-    row["Name"] = input("Name: ")
-    row["Email"] = input("Email Address: ")
-    row["Phone"] = input("Phone Numbers: ").split(" ")
-    row["StoreID"] = input("Store ID: ")
-    row["Type"] = input("Type of Employee: ")
-    row["Salary"] = float(input("Salary: "))
+    id=input("Enter HRID whose salary you want to see:")
+    print("The Employee with ID ",id ,"has a salary of", )
 
-    for i in range(len(row["Phone"])):
-	    query1 = "INSERT INTO HR(HRID, Name, Email, PhoneNumber) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["Name"], row["Email"], row["Phone"][i])
-    	cur.execute(query1)
-    	con.commit()
-
-    query2 = "INSERT INTO EMPLOYEE(HRID, StoreID, TypeOfEmployee, SALARY) VALUES('%s', '%s', '%s', '%d')" %(row["HRID"], row["StoreID"], row["Type"], row["SALARY"])
-
-    cur.execute(query2)
-    con.commit()
+    query = "SELECT SALARY FROM EMPLOYEE WHERE HRID = '%s'"
+    cur.execute(query)
+    blah = cur.fetchall()
+    blah=int(blah[0])
+    print blah
     return
-
-
-
